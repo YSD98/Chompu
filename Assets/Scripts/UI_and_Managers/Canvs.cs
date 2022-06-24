4b545efa9f941822fa617df38ec46126e78210d7
Yash <62443679+YSD98@users.noreply.github.com>
2022-06-24 16:28:27 +0530

    Modified UI and BG

--- Assets/Scripts/UI_and_Managers/Canvs.cs
+++ Assets/Scripts/UI_and_Managers/Canvs.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Canvs : MonoBehaviour
 {
@@ -23,5 +25,13 @@ public class Canvs : MonoBehaviour
         inGameUI.SetActive(false);
         gameOver.SetActive(true);
     }
-    public void Menu(){SceneManager.LoadScene(0);}
+
+    public void Quit()
+    {
+        #if UNITY_EDITOR
+                EditorApplication.ExitPlaymode();
+        #else
+                Application.Quit(); // original code to quit Unity player
+        #endif
+    }
 }
--- Assets/Scripts/UI_and_Managers/MenuManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MenuManager : MonoBehaviour
-{
-    public void play(){SceneManager.LoadScene(1);}
-    public void Exit(){Application.Quit();}
-}
